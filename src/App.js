9ddbe937ffc52e0e1f1671e4faf10937091d8913
skmcmannis <mcmannis@oregonstate.edu>
2020-08-26 13:51:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Ping extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      downloadSpeeds: [],
+      ping: 0,
+      pingTimer: 0,
+    }
+  }
+
+  getPing() {
+    return (Math.random() * 100).toFixed(2);
+  }
+
+  updatePing() {
+    let downloadSpeeds;
+    if (this.state.downloadSpeeds.length > 9) {
+      downloadSpeeds = this.state.downloadSpeeds.slice(1, 10);
+    } else {
+      downloadSpeeds = this.state.downloadSpeeds.slice(0, 9);
+    }
+    this.ping = this.getPing();
+
+    this.setState({
+      downloadSpeeds: downloadSpeeds.concat(this.ping),
+    });
+  }
+
+  componentDidMount() {
+    this.updatePing(this.state);
+
+    this.pingTimer = setInterval(() => {this.updatePing()}, 10000);
+  }
+
+  render() {
+    const downloadSpeeds = this.state.downloadSpeeds;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Current download speed: {downloadSpeeds[downloadSpeeds.length - 1]} mb/s.
+          </p>
+        </header>
+      </div>
+    ); 
+  }
 }
 
-export default App;
+ReactDOM.render (
+  <Ping />,
+  document.getElementById('root')
+);
+
+export default Ping;
